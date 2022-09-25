8e6efdd91770c16109b761391241fb7d2b12ec77
Anil <santosh5april@gmail.com>
2022-09-25 12:11:59 -0400

    remove bitch

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -22,6 +22,8 @@ public class Greeter {
   public static void main(String args[]){
     
 	  System.out.println("Hello from Main Method!!! Bitches");
-    System.out.println("Hello from Main Method!!! Bitche Branch");
+    
+    
+
   }
 }
